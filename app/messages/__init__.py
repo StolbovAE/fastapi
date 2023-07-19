4cdc40ecc8f56819b9a9626c2c73942c36e81dc6
Андрей <stolbov.andrei2012@yandex.ru>
2023-07-19 12:10:39 +0300

    updated requirements, logging in messages. + db migration

--- app/messages/__init__.py
+++ app/messages/__init__.py
@@ -1,14 +1,12 @@
 import logging
 
-from app import logger
-from typing import Union
-from fastapi import Request, Depends, HTTPException, APIRouter, status, UploadFile, Form
 from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
+from fastapi import APIRouter
 
+from app import config
 from app.messages.db_handler import MessageDbTable
 from app.messages.models import CreateMessageRequest
 from app.validators.response_validators import PayloadResponse
-from app import config
 
 messages = APIRouter()
 
@@ -38,6 +36,7 @@ async def startup_event():
         message_data = CreateMessageRequest(**received_message)
 
         response = MessageDbTable().create(message_data)
+        logging.debug(f"Got response: {response}")
     await consumer.stop()
 
 
